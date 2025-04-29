0c4b671eb8d70b727a12c3b4b31a80f4ad49a5d6
iamanggi <anggipuspita452@gmail.com>
2025-04-29 18:21:26 +0700

    memperbaiki halaman login dan menambah logo

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1_flutter/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -25,131 +26,148 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Form( 
         key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Selamat datang kembali'.toUpperCase(),
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+        child: Center(
+          child: SingleChildScrollView( 
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(image: AssetImage('assets/images/logo.png'), 
+                width: 400,
+                height: 180,),
+                const SizedBox(height: 10,),
+                Text(
+                  'Selamat datang kembali'.toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0),
-                child: 
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Email', 
-                    style: TextStyle(
-                      // fontSize: 16,
-                      fontWeight: FontWeight.bold
-                    ),),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: TextFormField(
-                        controller: emailController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(18))),
-                          // labelText: 'Email',
-                          hintText: 'Email',
-                          prefixIcon: Icon(Icons.email_rounded),
+                
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: 
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Email', 
+                      style: TextStyle(
+                        // fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: TextFormField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(18))),
+                            // labelText: 'Email',
+                            hintText: 'Email',
+                            prefixIcon: Icon(Icons.email_rounded),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Username tidak boleh kosong';
+                            }
+                            return null;
+                          },
                         ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Username tidak boleh kosong';
-                          }
-                          return null;
-                        },
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('password', style: TextStyle(fontWeight: FontWeight.bold),),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: TextFormField(
-                        controller: passwordController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(18))),
-                          labelText: 'Password',
-                          hintText: 'Masukkan Password',
-                          prefixIcon: Icon(Icons.lock),
-                          suffixIcon: InkWell(
-                            onTap: () {
-                              setState(() {
-                                _obscurePassword = !_obscurePassword;
-                              });
-                            },
-                            child: Icon(
-                              _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('password', style: TextStyle(fontWeight: FontWeight.bold),),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: TextFormField(
+                          controller: passwordController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(18))),
+                            labelText: 'Password',
+                            hintText: 'Masukkan Password',
+                            prefixIcon: Icon(Icons.lock),
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                setState(() {
+                                  _obscurePassword = !_obscurePassword;
+                                });
+                              },
+                              child: Icon(
+                                _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                              ),
                             ),
                           ),
+                          obscureText: _obscurePassword,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Password tidak boleh kosong';
+                            }
+                            return null;
+                          },
                         ),
-                        obscureText: _obscurePassword,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Password tidak boleh kosong';
-                          }
-                          return null;
-                        },
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0),
-                child: SizedBox(
-                  width: 500,
-                  height: 50,
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Berhasil Masuk!')),
-                        );
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 97, 153, 199),
-                    ),
-                    label: Text(
-                      'Masuk',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: SizedBox(
+                    width: 500,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => HomePage(
+                              nama: emailController.text,
+                            )),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Berhasil Masuk!')),
+                          );
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF003A60),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15), 
+                          ),
+                      ),
+                      child: Text(
+                        'Masuk',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Belum Memiliki Akun? Silahkan'),
-                  TextButton(onPressed: (){}
-                  , child: Text('Daftar Disini!',
-                  style: TextStyle(color: const Color.fromARGB(255, 21, 119, 200)),))
-                ],
-              )
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Belum Memiliki Akun? Silahkan'),
+                    TextButton(onPressed: (){}
+                    , child: Text('Daftar Disini!',
+                    style: TextStyle(color: Color(0xFF003A60))))
+                  ],
+                )
+              ],
+            ),
           ),
+                ),
         ),
-      ),
+      )
     );
   }
 }
